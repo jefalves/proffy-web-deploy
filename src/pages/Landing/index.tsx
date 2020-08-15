0f28411c689ba9c43fe81d6f75460960af27f9a2
Jeferson Alves <jeferson.alves@fxdata.com.br>
2020-08-14 21:49:49 -0300

    commit test

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -28,7 +28,7 @@ function Landing() {
       <div id="page-landing-content" className="container">
         <div className="logo-container">
           <img src={logoImg} alt="Proffy" />
-          <h2>Sua plataforma de estudos online.</h2>
+          <h2>Sua plataforma de estudos online</h2>
         </div>
 
         <img 
