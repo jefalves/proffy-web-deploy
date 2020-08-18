aca8bbb4e271cc4d6d2f8d1979943ad9742b7322
Jeferson Alves <jeferson.alves@fxdata.com.br>
2020-08-18 11:48:26 -0300

    clear form after submit

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,44 +1,242 @@
-import React from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
+import React, { useState, FormEvent, useCallback } from 'react';
+// import { useHistory } from 'react-router-dom'
+
+import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Textarea from '../../components/Textarea';
+
+import Select from '../../components/Select';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
+
 import api from '../../services/api';
+
+import './styles.css';
+
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
-type FormValues = {
-  name: string;
-  email: string;
-  avatar: string;
-  whatsapp: string;
-  bio: string;
-};
-
 function TeacherForm() {
-  const { register, handleSubmit } = useForm<FormValues>();
+  // const history = useHistory();
 
-  const onSubmit: SubmitHandler<FormValues> = data => {
-    api.post('classes', data).then(() => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [avatar, setAvatar] = useState('');
+  const [whatsapp, setWhatsapp] = useState('');
+  const [bio, setBio] = useState('');
+  const [subject, setSubject] = useState('');
+  const [cost, setCost] = useState('');
+  const [scheduleItems, setScheduleItems] = useState([
+    { week_day: 0, from: '', to: '' }
+  ]);
+
+  function addNewScheduleItem() {
+    setScheduleItems([
+      ...scheduleItems,
+      { week_day: 0, from: '', to: '' }
+    ]);
+  }
+
+  function setScheduleItemValue(position: number, field: string, value: string) {
+    const updatedScheduleItems = scheduleItems.map((scheduleItem, index) => {
+      if (index === position) {
+        return { ...scheduleItem, [field]: value };
+      }
+
+      return scheduleItem;
+    });
+
+    setScheduleItems(updatedScheduleItems);
+  }
+
+  
+  function handleCreateClass(e: FormEvent) {
+    e.preventDefault();
+
+    api.post('classes', {
+      name,
+      avatar,
+      email,
+      whatsapp,
+      bio,
+      subject,
+      cost: Number(cost),
+      schedule: scheduleItems
+    }).then(() => {
       toast.success("Cadastro realizado com sucesso!");
+      cleanupForm();
+      // history.push('/');
     }).catch(() => {
       toast.error("Erro no cadastro!");
-    })  
-  };
+    })
+  }
+
+
+  // Clear fields after submit
+
+  const cleanupForm = useCallback(() => {
+    setName('');
+    setEmail('');
+    setAvatar('');
+    setWhatsapp('');
+    setBio('');
+    setSubject('');
+    setCost('');
+    // só chamar todas as funções que são estados que preenchem os campos
+  }, [])
+
 
   return (
-    <main>
-    <ToastContainer />
-    <form onSubmit={handleSubmit(onSubmit)}>
-      nome
-      <input name="name" type="text" ref={register} />email
-      <input name="email" type="email" ref={register} />whatsapp
-      <input name="whatsapp" type="text" ref={register} />avatar
-      <input name="avatar" type="text" ref={register} />bio
-      <input name="bio" type="text" ref={register} />
+    <div id="page-teacher-form" className="container">
+      <PageHeader 
+        title="Que incrível que você quer dar aulas."
+        description="O primeiro passo é preencher esse formulário de inscrição"
+      />
 
-      <input type="submit" />
-    </form>
-    </main>
-  );
+      <main>
+      <ToastContainer />
+
+        <form onSubmit={handleCreateClass}>
+          <fieldset>
+            <legend>Seus dados</legend>
+
+            <Input 
+              name="name" 
+              label="Nome completo" 
+              value={name}
+              required
+              onChange={(e) => { setName(e.target.value) }}
+            />
+
+            <Input 
+              name="email" 
+              label="Email" 
+              value={email}
+              required
+              onChange={(e) => { setEmail(e.target.value) }}
+            />
+
+ 
+
+            <Input 
+              name="avatar" 
+              label="Avatar"
+              value={avatar}
+              required
+              onChange={(e) => { setAvatar(e.target.value) }}
+            />
+
+            <Input 
+              name="whatsapp" 
+              label="WhatsApp"
+              value={whatsapp}
+              required
+              onChange={(e) => { setWhatsapp(e.target.value) }}
+            />
+
+            <Textarea 
+              name="bio" 
+              label="Biografia"
+              required
+              value={bio}
+              onChange={(e) => { setBio(e.target.value) }}
+            />
+          </fieldset>
+
+          <fieldset>
+            <legend>Sobre a aula</legend>
+
+            <Select 
+              name="subject" 
+              label="Matéria"
+              value={subject}
+              required
+              onChange={(e) => { setSubject(e.target.value) }}
+              options={[
+                { value: 'Artes', label: 'Artes' },
+                { value: 'Biologia', label: 'Biologia' },
+                { value: 'Ciências', label: 'Ciências' },
+                { value: 'Educação física', label: 'Educação física' },
+                { value: 'Física', label: 'Física' },
+                { value: 'Geografia', label: 'Geografia' },
+                { value: 'História', label: 'História' },
+                { value: 'Matemática', label: 'Matemática' },
+                { value: 'Português', label: 'Português' },
+                { value: 'Química', label: 'Química' },
+              ]}
+            />
+            
+            <Input 
+              name="cost" 
+              label="Custo da sua hora por aula"
+              required
+              value={cost}
+              onChange={(e) => { setCost(e.target.value) }}
+            />
+          </fieldset>
+
+          <fieldset>
+            <legend>
+              Horários disponíveis
+              <button type="button" onClick={addNewScheduleItem}>
+                + Novo horário
+              </button>
+            </legend>
+            
+            {scheduleItems.map((scheduleItem, index) => {
+              return (
+                <div key={scheduleItem.week_day} className="schedule-item">
+                  <Select 
+                    name="week_day" 
+                    label="Dia da semana"
+                    value={scheduleItem.week_day}
+                    onChange={e => setScheduleItemValue(index, 'week_day', e.target.value)}
+                    options={[
+                      { value: '0', label: 'Domingo' },
+                      { value: '1', label: 'Segunda-feira' },
+                      { value: '2', label: 'Terça-feira' },
+                      { value: '3', label: 'Quarta-feira' },
+                      { value: '4', label: 'Quinta-feira' },
+                      { value: '5', label: 'Sexta-feira' },
+                      { value: '6', label: 'Sábado' },
+                    ]}
+                  />
+                  <Input 
+                    name="from" 
+                    label="Das" 
+                    type="time"
+                    value={scheduleItem.from}
+                    onChange={e => setScheduleItemValue(index, 'from', e.target.value)}
+                  />
+
+                  <Input 
+                    name="to" 
+                    label="Até" 
+                    type="time"
+                    value={scheduleItem.to}
+                    onChange={e => setScheduleItemValue(index, 'to', e.target.value)}
+                  />
+
+                </div>
+              );
+            })}
+          </fieldset>
+
+          <footer>
+            <p>
+              <img src={warningIcon} alt="Aviso importante" />
+              Importante! <br />
+              Preencha todos os dados
+            </p>
+            <button type="submit">
+              Salvar cadastro
+            </button>
+          </footer>
+        </form>
+      </main>
+    </div>
+  )
 }
 
 export default TeacherForm;
